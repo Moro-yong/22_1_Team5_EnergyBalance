db0586a0f82f3211bb00f5bad5b796d2597d3265
Jaeho <benteke@naver.com>
2022-02-17 18:50:28 +0900

    Add: 초기 셋팅 완료

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from './Router';
+import GlobalStyle from './styles/GlobalStyle';
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <Router />
+  </>,
+  document.getElementById('root')
+);
